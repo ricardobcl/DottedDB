867ed8df160fad08a6fddf650e998807524cccf7
ricardobcl <tome.wave@gmail.com>
2015-07-20 12:27:47 +0100

    Try catch for the exometer stater.

--- src/dotted_db_app.erl
+++ src/dotted_db_app.erl
@@ -40,11 +40,6 @@ stop(_State) ->
     ok.
 
 start_stats() ->
-    %% StatsD
-    % ReportOptions = [{host, "localhost"},
-    %                  {port, 8125}],
-    % ok = exometer_report:add_reporter(exometer_report_statsd, ReportOptions),
-    
 
     %% Graphite
     % {ok, Host} = inet:gethostname(),
@@ -54,62 +49,71 @@ start_stats() ->
                      {host, "localhost"},
                      {port, 2003},
                      {api_key, "dotted_db_stats_ric"}],
-    ok = exometer_report:add_reporter(exometer_report_graphite, ReportOptions),
-
-
-
-    % VM memory.
-    % total = processes + system.
-    % processes = used by Erlang processes, their stacks and heaps.
-    % system = used but not directly related to any Erlang process.
-    % atom = allocated for atoms (included in system).
-    % binary = allocated for binaries (included in system).
-    % ets = allocated for ETS tables (included in system).
-    ok = exometer:new([erlang, memory],
-                      {function, erlang, memory, ['$dp'], value,
-                       [total, processes, system, atom, binary, ets]}),
-    ok = exometer_report:subscribe(exometer_report_graphite,
-                                   [erlang, memory],
-                                   [total, processes, system, atom, binary,
-                                    ets], ?INTERVAL),
-
-    % process_count = current number of processes.
-    % port_count = current number of ports.
-    ok = exometer:new([erlang, system],
-                      {function, erlang, system_info, ['$dp'], value,
-                       [process_count, port_count]}),
-    ok = exometer_report:subscribe(exometer_report_graphite,
-                                   [erlang, system],
-                                   [process_count, port_count], ?INTERVAL),
-
-    % The number of processes that are ready to run on all available run queues.
-    ok = exometer:new([erlang, statistics],
-                      {function, erlang, statistics, ['$dp'], value,
-                       [run_queue]}),
-    ok = exometer_report:subscribe(exometer_report_graphite,
-                                   [erlang, statistics],
-                                   [run_queue], ?INTERVAL),
-
-
-    % 
-    ok = exometer:new([dotted_db, glc, entries, length], histogram),
-    ok = exometer_report:subscribe(exometer_report_graphite,
-                                   [dotted_db, glc, entries, length],
-                                   [min, max, median, mean, 95, 99, 999], 
-                                   ?INTERVAL),
-    % 
-    ok = exometer:new([dotted_db, sync, total], counter),
-    ok = exometer_report:subscribe(exometer_report_graphite,
-                                   [dotted_db, sync, total],
-                                   value, 
-                                   ?INTERVAL),
-
-    ok = exometer:new([dotted_db, sync, relevant_keys], histogram),
-    ok = exometer_report:subscribe(exometer_report_graphite,
-                                   [dotted_db, sync, relevant_keys],
-                                   [min, max, median, mean, 95, 99, 999], 
-                                   ?INTERVAL),
     
-
+    try 
+        ok = exometer_report:add_reporter(exometer_report_graphite, ReportOptions),
+        % VM memory.
+        % total = processes + system.
+        % processes = used by Erlang processes, their stacks and heaps.
+        % system = used but not directly related to any Erlang process.
+        % atom = allocated for atoms (included in system).
+        % binary = allocated for binaries (included in system).
+        % ets = allocated for ETS tables (included in system).
+        ok = exometer:new([erlang, memory],
+                          {function, erlang, memory, ['$dp'], value,
+                           [total, processes, system, atom, binary, ets]}),
+        ok = exometer_report:subscribe(exometer_report_graphite,
+                                       [erlang, memory],
+                                       [total, processes, system, atom, binary,
+                                        ets], ?INTERVAL),
+    
+        % process_count = current number of processes.
+        % port_count = current number of ports.
+        ok = exometer:new([erlang, system],
+                          {function, erlang, system_info, ['$dp'], value,
+                           [process_count, port_count]}),
+        ok = exometer_report:subscribe(exometer_report_graphite,
+                                       [erlang, system],
+                                       [process_count, port_count], ?INTERVAL),
+    
+        % The number of processes that are ready to run on all available run queues.
+        ok = exometer:new([erlang, statistics],
+                          {function, erlang, statistics, ['$dp'], value,
+                           [run_queue]}),
+        ok = exometer_report:subscribe(exometer_report_graphite,
+                                       [erlang, statistics],
+                                       [run_queue], ?INTERVAL),
+    
+    
+        % 
+        ok = exometer:new([dotted_db, glc, entries, length], histogram),
+        ok = exometer_report:subscribe(exometer_report_graphite,
+                                       [dotted_db, glc, entries, length],
+                                       [min, max, median, mean, 95, 99, 999], 
+                                       ?INTERVAL),
+        % 
+        ok = exometer:new([dotted_db, sync, total], counter),
+        ok = exometer_report:subscribe(exometer_report_graphite,
+                                       [dotted_db, sync, total],
+                                       value, 
+                                       ?INTERVAL),
+    
+        ok = exometer:new([dotted_db, sync, relevant_keys], histogram),
+        ok = exometer_report:subscribe(exometer_report_graphite,
+                                       [dotted_db, sync, relevant_keys],
+                                       [min, max, median, mean, 95, 99, 999], 
+                                       ?INTERVAL)
+    of
+        _ -> true
+    catch
+        throw:Throw -> 
+            lager:warning("Throw: ~p!", [Throw]);
+        exit:Exit   -> 
+            lager:warning("Exit: ~p!",  [Exit]);
+        error:Error -> 
+            lager:warning("Error: ~p!", [Error]);
+        _:_         ->
+            lager:warning("Problem with exometer!")
+    end,
 
     ok.
